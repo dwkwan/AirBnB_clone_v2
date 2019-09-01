4054a305c53cf61c429a350a90e22cc373126365
David Kwan <dwkwan1989@gmail.com>
2019-09-01 20:13:35 +0000

    Don't run code when imported

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,4 +8,6 @@ app = Flask(__name__)
 def hello_world():
     """Returns the string Hello HBNB"""
     return 'Hello HBNB!'
-app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
